48dd51985df7277bd1e613feaf25108dab05e67c
Fx137 <flufeu@gmail.com>
2020-03-11 21:13:17 +0100

    initial commit

---
+++ netCore/MqttSniffer/Service/Publisher.cs
@@ -0,0 +1,56 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Subscribing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MqttSniffer.Service
+{
+    interface IPublisher
+    {
+        Task Initialize();
+        Task SendMessage(string topic, string message);
+    }
+
+    class Publisher : IPublisher
+    {
+        private readonly string url;
+        private IMqttClient mqttClient;
+
+        public Publisher(string url)
+        {
+            this.url = url;
+        }
+
+        public async Task Initialize()
+        {
+            var factory = new MqttFactory();
+            mqttClient = factory.CreateMqttClient();
+
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer(url, 1883)
+                .Build();
+            
+            mqttClient.UseApplicationMessageReceivedHandler(e =>
+            {
+                Console.WriteLine($"+ Message = {e.ApplicationMessage.Topic} => {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+            });
+
+            mqttClient.UseConnectedHandler(async e =>
+            {
+                await mqttClient.SubscribeAsync(new TopicFilterBuilder().WithTopic("FX/#").Build());
+            });
+
+            await mqttClient.ConnectAsync(options, CancellationToken.None);
+        }
+
+        public async Task SendMessage(string topic, string message)
+        {
+            await mqttClient.PublishAsync(topic, message);
+        }
+    }
+}
---
+++ netCore/MqttSniffer/ViewModels/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Caliburn.Micro;
+using MqttSniffer.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MqttSniffer.ViewModels
+{
+    internal interface IMainViewModel
+    {
+        string Output { get; set; }
+        string Message { get; set; }
+        string Topic { get; set; } 
+        Task SendMessage();
+
+    }
+
+    internal class MainViewModel : Screen, IMainViewModel
+    {
+        private string console;
+        private readonly IPublisher publisher;
+
+        public string Output
+        {
+            get => console; set
+            {
+                console = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        public string Message { get; set; } = "message";
+        public string Topic { get; set; } = "FX";
+
+        public MainViewModel(IPublisher publisher)
+        {
+            this.publisher = publisher;
+        }
+
+        public async Task SendMessage()
+        {
+            await publisher.SendMessage(Topic, Message);
+        }
+    }
+}
